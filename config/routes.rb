a59fa54c05ff5145043754e340bdc746c69edd69
tomekl007 <tomekl007@gmail.com>
2013-05-03 09:31:12 -0700

    add login function, admin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,44 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 Depot::Application.routes.draw do
-  resources :orders
+  get 'admin' => 'admin#index'
 
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+  resources :users
+
+  resources :orders
 
   resources :line_items
 
-
   resources :carts
 
-
   get "store/index"
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
-  resources :products
 
-
-
-
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -64,18 +88,14 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => "welcome#index"
   root :to => 'store#index', :as => 'store'
+
+  # ...
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-
-  resources :products do
-    get :who_bought, :on => :member
-  end
-
-
+  # match ':controller(/:action(/:id(.:format)))'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503100748) do
+ActiveRecord::Schema.define(:version => 20130503103439) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -61,5 +61,13 @@ ActiveRecord::Schema.define(:version => 20130503100748) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
 end
